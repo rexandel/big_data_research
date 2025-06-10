cb65c70927491482d6d1bbc4cab277a65a86fe9a
Alexander Sidorenko <xarendel@gmail.com>
2025-06-10 23:34:03 +0300

    Task 2.5 - 2.6 (Bug fix & create subsets of data)

--- student_survey_analysis/analysis.R
+++ student_survey_analysis/analysis.R
@@ -5,7 +5,6 @@ data <- data.frame(survey)
 data
 
 # --- Getting surnames and genres ---
-
 surnames <- data[, 1]
 surnames
 
@@ -13,12 +12,10 @@ genres <- names(data[, -1])
 genres
 
 # --- Normalization of data ---
-
 data[, sapply(data, is.numeric)] <- data[, sapply(data, is.numeric)] / 10
 data
 
 # --- Calculating metrics ---
-
 min_values <- sapply(data[, sapply(data, is.numeric)], min, na.rm = TRUE)
 min_values
 
@@ -29,7 +26,6 @@ mean_values <- sapply(data[, sapply(data, is.numeric)], mean, na.rm = TRUE)
 mean_values
 
 # --- Analysis of distribution of respondents by preference level ---
-
 count_of_high_ratings <- colSums(data[, sapply(data, is.numeric)] > 0.7, na.rm = TRUE)
 count_of_high_ratings
 
@@ -37,7 +33,6 @@ count_of_low_ratings <- colSums(data[, sapply(data, is.numeric)] < 0.3, na.rm = 
 count_of_low_ratings
 
 # --- Working with missing data ---
-
 data_mean_input <- data
 
 numeric_cols <- sapply(data_mean_input, is.numeric)
@@ -47,14 +42,37 @@ data_mean_input[, numeric_cols] <- lapply(data_mean_input[, numeric_cols], funct
 })
 data_mean_input
 
+data_zero <- data
+numeric_cols <- sapply(data_zero, is.numeric)
+data_zero[, numeric_cols] <- lapply(data_zero[, numeric_cols], function(x) {
+  x[is.na(x)] <- 0
+  x
+})
+data_zero
+
 data_delete <- na.omit(data)
 data_delete
 
 data <- data_mean_input
 
 # --- Sorting data set ---
-sorted_genres_asc <- sort(mean_values)
+sorted_genres_asc <- sort(mean_values, decreasing = FALSE)
 sorted_genres_asc
 
 sorted_genres_desc <- sort(mean_values, decreasing = TRUE)
 sorted_genres_desc
+
+# --- Creating subsets of data ---
+sci_fi_high <- data[data$Sci.Fi > 0.7, ]
+sci_fi_high
+
+students_sci_fi_high <- sci_fi_high[, 1]
+students_sci_fi_high
+
+musical_low <- data[data$Musical > 0.7, ]
+musical_low
+
+students_musical_low <- musical_low[, 1]
+students_musical_low
+
+
